f1974fdf8766766be180076ada4b48d62d86b439
김명구 <myeonggu.kim@kakao.com>
2024-08-02 10:22:23 +0900

    RestHighLevelClient 추가

--- common-elastic/src/main/java/spring/app/common/elastic/config/ElasticConfig.java
+++ common-elastic/src/main/java/spring/app/common/elastic/config/ElasticConfig.java
@@ -1,11 +1,20 @@
 package spring.app.common.elastic.config;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticConfig {
   
-//  @Bean
+  @Bean
+  RestHighLevelClient restHighLevelClient() {
+    RestClientBuilder builder = RestClient.builder(
+        new HttpHost("localhost", 9200, "https"));
+    return new RestHighLevelClient(builder);
+  }
   
 }
