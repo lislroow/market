7896ef792e61aaf2f8c802a219f3ba0a95b8fcec
김명구 <myeonggu.kim@kakao.com>
2024-08-02 10:00:17 +0900

    common-elastic 추가

---
+++ common-elastic/src/main/java/spring/app/common/elastic/config/ElasticConfig.java
@@ -0,0 +1,11 @@
+package spring.app.common.elastic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticConfig {
+  
+//  @Bean
+  
+}
