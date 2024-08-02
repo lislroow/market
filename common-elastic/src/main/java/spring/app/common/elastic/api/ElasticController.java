bdd5292c071852f7dc3782207ebfdd0559a36e2c
김명구 <myeonggu.kim@kakao.com>
2024-08-02 13:51:16 +0900

    조회 예시 추가

--- common-elastic/src/main/java/spring/app/common/elastic/api/ElasticController.java
+++ common-elastic/src/main/java/spring/app/common/elastic/api/ElasticController.java
@@ -6,6 +6,7 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,17 @@ public class ElasticController {
   public String hello() throws Exception {
     SearchRequest request = new SearchRequest("fibonacci");
     SearchSourceBuilder builder = new SearchSourceBuilder();
-    builder.query(QueryBuilders.matchAllQuery());
+    
+    String[] includeFields = new String[] { "days", "num" };
+    String[] excludeFields = new String[] { };
+    builder.fetchSource(includeFields, excludeFields);
+    
+    builder.sort("days", SortOrder.ASC);
+    builder.from(0);
+    //builder.size(3);
+    
+    //builder.query(QueryBuilders.matchAllQuery());
+    builder.query(QueryBuilders.rangeQuery("days").from(3).to(6));
     request.source(builder);
     
     SearchResponse response = client.search(request, RequestOptions.DEFAULT);
