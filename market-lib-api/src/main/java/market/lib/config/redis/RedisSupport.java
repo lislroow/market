2cfd3eac7a7541d3dbd5a75827b4bc9906c14bb1
김명구 <myeonggu.kim@kakao.com>
2024-10-12 18:39:53 +0900

    코드 정리

--- market-lib-api/src/main/java/market/lib/config/redis/RedisSupport.java
+++ market-lib-api/src/main/java/market/lib/config/redis/RedisSupport.java
@@ -5,41 +5,38 @@ import java.util.List;
 import java.util.Set;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class RedisSupport {
 
-  private final HashOperations<String, String, Object> hashOps;
-  
+  private final RedisTemplate<String, Object> redisTemplate;
   private final ModelMapper modelMapper;
   
-  public RedisSupport(RedisTemplate<String, Object> redisTemplate, ModelMapper modelMapper) {
-    this.hashOps = redisTemplate.opsForHash();
-    this.modelMapper = modelMapper;
-  }
-  
+  // hash
   public void setHash(String key, String hashKey, Object val) {
-    hashOps.put(key, hashKey, val);
+    this.redisTemplate.opsForHash().put(key, hashKey, val);
   }
   
   @SuppressWarnings("unchecked")
   public <T> T getHash(String key, String hashKey, Class<T> type) {
-    return (T) hashOps.get(key, hashKey);
+    return (T) this.redisTemplate.opsForHash().get(key, hashKey);
   }
   
   public void deleteHash(String key, String hashKey) {
     if (hashKey != null && hashKey.indexOf("*") > -1) {
-      Set<String> hkeySet = hashOps.keys(key);
-      hkeySet.forEach(hkey -> hashOps.delete(key, hkey));
+      Set<Object> hkeySet = this.redisTemplate.opsForHash().keys(key);
+      hkeySet.forEach(hkey -> this.redisTemplate.opsForHash().delete(key, hkey));
     } else {
-      hashOps.delete(key, hashKey);
+      this.redisTemplate.opsForHash().delete(key, hashKey);
     }
   }
   
-  public <T> List<T> getAll(String key, Class<T> type) {
+  public <T> List<T> getHashAll(String key, Class<T> type) {
     List<T> list = new ArrayList<>();
-    for (Object object : hashOps.values(key)) {
+    for (Object object : this.redisTemplate.opsForHash().values(key)) {
       if (object == null) {
         list.add(null);
       } else {
@@ -49,11 +46,11 @@ public class RedisSupport {
     return list;
   }
   
-  public boolean isEmpty(String key, String hashKey) {
-    return hashOps.hasKey(key, hashKey);
+  public boolean isHashEmpty(String key, String hashKey) {
+    return this.redisTemplate.opsForHash().hasKey(key, hashKey);
   }
   
-  public void remove(String key, String hashKey) {
-    hashOps.delete(key, hashKey);
+  public void removeHash(String key, String hashKey) {
+    this.redisTemplate.opsForHash().delete(key, hashKey);
   }
 }
