0ee84921294b273109143d7152303eca1ee2843d
myeonggu.kim <hi@mgkim.net>
2024-11-12 17:22:40 +0900

    misc: 코드 정리

--- market-lib-const/src/main/java/market/lib/constant/Constant.java
+++ market-lib-const/src/main/java/market/lib/constant/Constant.java
@@ -5,19 +5,18 @@ public class Constant {
   private Constant() {}
 
   public static final String BASE_PACKAGE = "market";
-  
   public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+  public static final String ENCODING_UTF8 = "utf-8";
+
+  public static final String X_TOKEN_ID = "X-TOKEN-ID";
+  public static final String ACCESS_TOKEN = "ACCESS-TOKEN";
+  public static final String REFRESH_TOKEN = "REFRESH-TOKEN";
+  
+  public static final String PUB_TOKEN_ID = "PUB-TOKEN-ID";
+  public static final String PUB_CAPTCHA_ENC = "PUB-CAPTCHA-ENC";
+  public static final String PUB_CAPTCHA_ANS = "PUB-CAPTCHA-ANS";
+  
   public static final String UPLOAD_BASE = "/upload";
   public static final String UPLOAD_BASE_PRODUCT = UPLOAD_BASE+"/product";
-  
-  public static final String X_TOKEN_ID = "X-TOKEN_ID";
-  public static final String ACCESS_TOKEN = "ACCESS_TOKEN";
-  public static final String REFRESH_TOKEN = "REFRESH_TOKEN";
-  
-  public static final String ENCODING_UTF8 = "utf-8";
-  
-  public static final String X_PUB_TOKEN = "X-PUB-TOKEN";
-  public static final String X_PUB_CAPTCHA_ENC = "X-PUB-CAPTCHA_ENC";
-  public static final String X_PUB_CAPTCHA_ANS = "X-PUB-CAPTCHA_ANS";
   
 }
