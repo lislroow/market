c50ad172f69c1d119d9b0b7d22f5ebd3089ca440
myeonggu.kim <hi@mgkim.net>
2024-11-17 15:10:16 +0900

    misc: main 클래스 annotation 정리

--- market-api-customer/src/main/java/market/CustomerMain.java
+++ market-api-customer/src/main/java/market/CustomerMain.java
@@ -2,13 +2,11 @@ package market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(
-    exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 @EnableTransactionManagement
 @EnableJpaRepositories
 @EnableFeignClients
--- market-api-inventory/src/main/java/market/InventoryMain.java
+++ market-api-inventory/src/main/java/market/InventoryMain.java
@@ -2,8 +2,14 @@ package market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableJpaRepositories
+@EnableFeignClients
 public class InventoryMain {
   
   public static void main(String[] args) {
