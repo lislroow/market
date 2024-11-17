9741290255d03db29a4e8ad07ed36ebc7fad2707
myeonggu.kim <hi@mgkim.net>
2024-11-17 14:45:49 +0900

    fix: MessageConverter 에서 LocalDateTime 을 ISO-8601 형식의 문자열로 직렬화하도록 설정

--- market-lib-api/src/main/java/market/config/WebMvcConfig.java
+++ market-lib-api/src/main/java/market/config/WebMvcConfig.java
@@ -1,13 +1,22 @@
 package market.config;
 
+import java.util.Collections;
+
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -44,5 +53,23 @@ public class WebMvcConfig implements WebMvcConfigurer {
     filterRegistrationBean.setFilter(new ForwardedHeaderFilter());
     filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
     return filterRegistrationBean;
+  }
+  
+  
+  @Bean
+  MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    return new MappingJackson2HttpMessageConverter(objectMapper);
+  }
+  
+  @Bean
+  HttpMessageConverters httpMessageConverters() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    HttpMessageConverter<?> converter = new MappingJackson2HttpMessageConverter(objectMapper);
+    return new HttpMessageConverters(false, Collections.singletonList(converter));
   }
 }
